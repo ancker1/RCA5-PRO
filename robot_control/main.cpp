2f48a178c48fe841501856a90ce8fc7b7c1c5d71
seht97 <soeha16@student.sdu.dk>
2018-11-05 22:36:54 -0800

    Fixed some oopsie conflicts

--- robot_control/main.cpp
+++ robot_control/main.cpp
@@ -184,11 +184,6 @@ int main(int _argc, char **_argv) {
   worldPublisher->WaitForConnection();
   worldPublisher->Publish(controlMessage);
 
-
-<<<<<<< HEAD
-
-
-
   float angle_max = arr[0];
   float angle_min = arr[1];
   float range_min = arr[2];
@@ -205,150 +200,6 @@ int main(int _argc, char **_argv) {
   float right_border = -M_PI/6;
   float right_border2 = -M_PI/2;
   float range_border = 0.35;
-=======
-	float angle_max     = arr[0];
-	float angle_min     = arr[1];
-	float range_min     = arr[2];
-	float range_max     = arr[3];
-	float left_border   = M_PI / 6;
-	float left_border2  = M_PI / 2;
-	float right_border  = -M_PI / 6;
-	float right_border2 = -M_PI / 2;
-	float range_border  = 0.35;
-  using namespace fl;
-	Engine *engine = new Engine;
-  engine->setName("SimpleTest");
-  engine->setDescription("First test of a Fuzzy Controller");
-
-  /*   INPUT VARIABLES   */
-	InputVariable *obstacle = new InputVariable;
-  obstacle->setName("Obstacle");
-  obstacle->setDescription("Indicates relative position of nearest obstacle");
-  obstacle->setEnabled(true);
-  obstacle->setRange(angle_min, angle_max);
-  obstacle->setLockValueInRange(false);
-
-	/*
-																													 obstacle->addTerm(new
-																															Rectangle("Left",
-																															left_border,
-																																					left_border2));
-																													 obstacle->addTerm(new
-																															Rectangle("OKLeft",
-																															left_border2,
-																																					angle_max));
-																													 obstacle->addTerm(new
-																															Rectangle("Front",
-																															right_border,
-																																					left_border));
-																													 obstacle->addTerm(new
-																															Rectangle("Right",
-																															right_border2,
-																																					right_border));
-																													 obstacle->addTerm(new
-																															Rectangle("OKRight",
-																															angle_min,
-																																					right_border2));
-	 */
-  obstacle->addTerm(new Rectangle("OKLeft", left_border2, angle_max));
-	obstacle->addTerm(new Ramp("Left", float(M_PI / 60), left_border2));
-
-  obstacle->addTerm(new Rectangle("OKRight", angle_min, right_border2));
-	obstacle->addTerm(new Rectangle("Right", right_border2, float(-M_PI / 60)));
-
-	obstacle->addTerm(new Trapezoid("Front", right_border, float(-M_PI / 60),
-																	float(M_PI / 60), left_border));
-
-  engine->addInputVariable(obstacle);
-
-	InputVariable *distance = new InputVariable;
-  distance->setName("Distance");
-  distance->setDescription("Distance to nearest obstacle");
-  distance->setEnabled(true);
-  distance->setRange(range_min, range_max);
-  distance->setLockValueInRange(false);
-
-	/*
-																													 distance->addTerm(new
-																															Rectangle("Close",
-																															range_min,
-																																					range_border));
-																													 distance->addTerm(new
-																															Rectangle("Far",
-																															range_border,
-																																					range_max));
-	 */
-  distance->addTerm(new Rectangle("Close", range_min, range_border));
-  distance->addTerm(new Triangle("Medium", range_border, 1));
-  distance->addTerm(new Ramp("Far", 0.8, range_max));
-  engine->addInputVariable(distance);
-
-  /*   OUTPUT VARIABLES  */
-	OutputVariable *Speed = new OutputVariable;
-  Speed->setName("Speed");
-  Speed->setDescription("Translational speed of robot");
-  Speed->setEnabled(true);
-	Speed->setRange(0, 1);
-  Speed->setLockValueInRange(false);
-  Speed->setAggregation(new Maximum);
-  Speed->setDefuzzifier(new Centroid(100));
-  Speed->setDefaultValue(0);
-  Speed->addTerm(new Rectangle("Go", 0.01, 1));
-  Speed->addTerm(new Rectangle("Stop", 0, 0.01));
-  engine->addOutputVariable(Speed);
-
-	OutputVariable *direction = new OutputVariable;
-  direction->setName("Direction");
-  direction->setDescription("Rotational speed of the robot");
-  direction->setEnabled(true);
-	direction->setRange(0, 0.5);
-  direction->setLockValueInRange(false);
-  direction->setAggregation(new Maximum);
-  direction->setDefuzzifier(new Centroid(100));
-  direction->setDefaultValue(0);
-  direction->addTerm(new Rectangle("Right", 0.01, 0.5));
-  direction->addTerm(new Rectangle("Front", 0, 0.01));
-  engine->addOutputVariable(direction);
-
-  /*   Ruleblock   */
-	RuleBlock *mamdani = new RuleBlock;
-  mamdani->setName("Mamdani");
-  mamdani->setDescription("");
-  mamdani->setEnabled(true);
-  mamdani->setConjunction(new Minimum);
-  mamdani->setDisjunction(fl::null);
-  mamdani->setImplication(new AlgebraicProduct);
-  mamdani->setActivation(new General);
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Far then Speed is Go and Direction is Front",
-										 engine));
-
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Medium and Obstacle is Front then Speed is Go and Direction is Right",
-										 engine));
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Medium and Obstacle is Left then Speed is Go and Direction is Front",
-										 engine)); // small_right
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Medium and Obstacle is Right then Speed is Go and Direction is Front",
-										 engine)); // small_left
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Medium and Obstacle is OKLeft then Speed is Go and Direction is Front",
-										 engine));
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Medium and Obstacle is OKRight then Speed is Go and Direction is Front",
-										 engine));
-
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Close and Obstacle is Front then Speed is Stop and Direction is Right",
-										 engine));
-	mamdani->addRule(Rule::parse(
-										 "if Distance is Close and Obstacle is Left then Speed is Stop and Direction is Right",
-										 engine));
-
-  engine->addRuleBlock(mamdani);
-
->>>>>>> CircleDetection
 
 	const int key_left  = 81;
 	const int key_up    = 82;
@@ -368,7 +219,7 @@ int main(int _argc, char **_argv) {
 
 
     controller->calcRelativeVectorToGoal(robot, goal);          // TEST
-    vector ans = controller->getRelativeVectorToGoal();         // TEST
+		vector ans = controller->getRelativeVectorToGoal();         // TEST
   //  std::cout << "Vector length: " << ans.length << std::endl;  // TEST
   //  std::cout << "Vector angle: " <<  ans.angle << std::endl;   // TEST
 
@@ -404,23 +255,14 @@ int main(int _argc, char **_argv) {
     /*************************************************************/
 
 
-<<<<<<< HEAD
     controller->setDistanceToClosestObstacle(range);
     controller->setAngleToClosestObstacle(angle);
 
     controller->calcRelativeVectorToGoal(robot, goal);          // Create method that takes robot orient, pos and goal pos.
-    vector ans = controller->getRelativeVectorToGoal();         // ^ should calculate the rel dist and angle directly.
-=======
-    speed = Speed->getValue();
-		dir   = direction->getValue();
+		vector ans = controller->getRelativeVectorToGoal();         // ^ should calculate the rel dist and angle directly.
 
-		// FL_LOG("Distance.input = " << Op::str(distance->getValue()) << "
-		// Obstacle.input = " << Op::str(obstacle->getValue()) << " Speed.output =
-		//  " << Op::str(Speed->getValue()) << "Direction.output = " <<
-		// Op::str(direction->getValue()) );
->>>>>>> CircleDetection
 
-    std::cout << "ang. bw: " << ans.angle << std::endl;
+		std::cout << "ang. bw: " << ans.angle << std::endl;
     std::cout << "robot orient: " << robot_oz << std::endl;
 
 
@@ -482,14 +324,6 @@ int main(int _argc, char **_argv) {
       //      dir *= 0.1;
     }
 
-<<<<<<< HEAD
-=======
-		if (key == 'w') speed += 0.5;
-		else if (key == 's') speed -= 0.5;
-		else if (key == 'a') dir -= 0.5;
-		else if (key == 'd') dir += 0.5;
-
->>>>>>> CircleDetection
     // Generate a pose
     ignition::math::Pose3d pose(double(speed), 0, 0, 0, 0, double(dir));
 
@@ -535,10 +369,6 @@ int main(int _argc, char **_argv) {
 
   }
 
-  // Make sure to shut everything down.
-<<<<<<< HEAD
-  gazebo::client::shutdown();
-=======
+	// Make sure to shut everything down.
 	gazebo::client::shutdown();
->>>>>>> CircleDetection
 }
